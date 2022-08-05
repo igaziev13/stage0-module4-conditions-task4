2889116bad0100bb6d508cedccdf363130bfa4e8
IGAZIEV <islam.gaziev@gmail.com>
2022-08-05 10:38:07 +0500

    Module #4.1. Task #4. Attempt #1.

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
-    public void printGreatest(int first, int second, int third) {
+    public static void printGreatest(int first, int second, int third) {
+        int maxNum;
+        maxNum = first > second ? first : second;
+        maxNum = maxNum > third ? maxNum : third;
+        System.out.println(maxNum);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxNum;
+        maxNum = first > second ? first : second;
+        maxNum = maxNum > third ? maxNum : third;
+        System.out.println(maxNum);
     }
 }
